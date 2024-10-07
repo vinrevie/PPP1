fb04d496f2ed4fdc3a309488e81bd35a77d56385
vinrevie <80602688+vinrevie@users.noreply.github.com>
2024-10-07 14:19:04 +0300

    commit

---
+++ ConsoleApp3/ConsoleApp3/ATS.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class ATS
+    {
+        private string adress;
+        private int numberOfAbonents;
+        private int price;
+        public string Adress {  get { return adress; } set { adress = value; } }
+        public int NumberOfAbonents { get { return numberOfAbonents; } set { numberOfAbonents = value; } }
+        public int Price {  set { price = value; } }
+        public int Total()
+        {
+           return numberOfAbonents * price;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("АТС по адресу: "+adress+ " содержит "+numberOfAbonents + " абонентов, всего сумма за всех абонентов= "+ Total());
+        }
+        public ATS(int num, int price, string adress)
+        {
+            numberOfAbonents = num;
+            this.price = price;
+            this.adress =adress;
+        }
+
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ATS ats1 = new ATS(710, 250, "ул. Политехническая, д. 27");
+            ats1.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
+//Здание – Отопительная система. 
